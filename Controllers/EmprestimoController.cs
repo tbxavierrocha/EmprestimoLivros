634538f849e5f0d05ad29d561d31a8da64c973aa
TULIO ROCHA <tuliofisiomec@outlook.com>
2023-07-08 00:13:39 -0300

    video2

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -7,14 +7,100 @@ namespace EmprestimoLivros.Controllers
     public class EmprestimoController : Controller
     {
         readonly private ApplicationDbContext _db;
-        public EmprestimoController(ApplicationDbContext db) 
-        { 
+        public EmprestimoController(ApplicationDbContext db)
+        {
             _db = db;
-        }   
+        }
         public IActionResult Index()
         {
             IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos; //_db.Emprestimos se refere à tabela
             return View(emprestimos);
         }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id) 
+        {
+            if (id == null || id ==0) 
+            { 
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);   
+
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModel emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id==id);
+
+            if(emprestimo == null) 
+            { 
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
